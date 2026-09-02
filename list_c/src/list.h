6ed7ea30ab7efe92b3a8f7b689b47abfdc1c748e
flynux <flynnzzz@users.noreply.github.com>
2026-09-02 18:50:58 +0200

    migration: updated projects started on android to work on linux

--- list_c/src/list.h
+++ list_c/src/list.h
@@ -1,5 +1,5 @@
 /*
- * list.h 
+ * list.h
  *
  * Generic linked list implementation/mock with
  * preprocessor directives.
@@ -7,55 +7,57 @@
 #ifndef LIST_H
 #define LIST_H
 
-#define QALLOC(type, n) \
-	(type*) malloc(n * sizeof(type))
-#define CHECK_ALLOC(ptr, msg) \
-	if (ptr == NULL) { \
-		perror(msg); \
-		exit(1); \
-	} 
-
 /*
  * Define flag to allow compilation
  */
 #ifdef LIST_TYPE
-#include <stdlib.h>
-#define DEFINE_LIST(T) \
-	typedef struct list_##T##_head { \
-		T *data; \
-		struct list_##T##_head *next; \
-	} list_##T; \
-	\
-	list_##T *append_##T(list_##T* list, const T* item) { \
-		list_##T *list_head = QALLOC(list_##T, 1); \
-		CHECK_ALLOC(list_head, "[ERROR] list head allocation"); \
-		list_head->data = QALLOC(T, 1); \
-		CHECK_ALLOC(list_head->data, "[ERROR] list data allocation"); \
-		memcpy(list_head->data, item, sizeof(T)); \
-		list_head->next = list; \
-		return list_head; \
-	} \
-	\
-	void printlst_##T(list_##T *list, const char* (*to_string)(const T *item)) { \
-		if (list != NULL) { \
-			printlst_##T(list->next, to_string); \
-			const char* str = to_string(list->data); \
-			printf("%s\n", str); \
-			free((void *)str); \
-		} \
-		return; \
-	} \
-	\
-	void freelst_##T(list_##T *list) { \
-		list_##T* head = list; \
-		while (head != NULL) { \
-			free((void *)(head->data)); \
-			head = head->next; \
-		} \
-		free((void *)list); \
-	}
 
-#define empty_list(T) (list_##T *) NULL
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define QALLOC(type, n) (type *)malloc(n * sizeof(type))
+#define CHECK_ALLOC(ptr, msg)                                                  \
+  if (ptr == NULL) {                                                           \
+    perror(msg);                                                               \
+    exit(1);                                                                   \
+  }
+#define DEFINE_LIST(T)                                                         \
+  typedef struct list_##T##_head {                                             \
+    T *data;                                                                   \
+    struct list_##T##_head *next;                                              \
+  } list_##T;                                                                  \
+                                                                               \
+  list_##T *append_##T(list_##T *list, const T *item) {                        \
+    list_##T *list_head = QALLOC(list_##T, 1);                                 \
+    CHECK_ALLOC(list_head, "[ERROR] list head allocation");                    \
+    list_head->data = QALLOC(T, 1);                                            \
+    CHECK_ALLOC(list_head->data, "[ERROR] list data allocation");              \
+    memcpy(list_head->data, item, sizeof(T));                                  \
+    list_head->next = list;                                                    \
+    return list_head;                                                          \
+  }                                                                            \
+                                                                               \
+  void printlst_##T(list_##T *list, const char *(*to_string)(const T *item)) { \
+    if (list != NULL) {                                                        \
+      printlst_##T(list->next, to_string);                                     \
+      const char *str = to_string(list->data);                                 \
+      printf("%s\n", str);                                                     \
+      free((void *)str);                                                       \
+    }                                                                          \
+    return;                                                                    \
+  }                                                                            \
+                                                                               \
+  void freelst_##T(list_##T *list) {                                           \
+    list_##T *head = list;                                                     \
+    while (head != NULL) {                                                     \
+      free((void *)(head->data));                                              \
+      head = head->next;                                                       \
+    }                                                                          \
+    free((void *)list);                                                        \
+  }
+
+#define empty_list(T) (list_##T *)NULL
 
 #endif // LIST_TYPE
 #endif // LIST_H
--- list_c/src/main.c
+++ list_c/src/main.c
@@ -1,60 +1,61 @@
-#define LIST_TYPE 
+#define LIST_TYPE
 #include "list.h"
 
 #define BUFFER_SIZE 100
 DEFINE_LIST(float);
 
 typedef struct person_t {
-	const char* name;
-	unsigned age;
+  const char *name;
+  unsigned age;
 } person;
 
 DEFINE_LIST(person);
 
-const char* float_to_string(const float* item) {
-	char* buffer = QALLOC(char, BUFFER_SIZE);
-	CHECK_ALLOC(buffer, "[ERROR] float to string buffer alloc");
-	snprintf(buffer, BUFFER_SIZE, "%.3f", *item);
-	return buffer;
+const char *float_to_string(const float *item) {
+  char *buffer = QALLOC(char, BUFFER_SIZE);
+  CHECK_ALLOC(buffer, "[ERROR] float to string buffer alloc");
+  snprintf(buffer, BUFFER_SIZE, "%.3f", *item);
+  return buffer;
 }
 
-const char* person_to_string(const person* item) {
-	char* buffer = QALLOC(char, BUFFER_SIZE);
-	CHECK_ALLOC(buffer, "[ERROR] person to string buffer alloc");
-	snprintf(buffer, BUFFER_SIZE, "%s, age %d", item->name, item->age);
-	return buffer;
+const char *person_to_string(const person *item) {
+  char *buffer = QALLOC(char, BUFFER_SIZE);
+  CHECK_ALLOC(buffer, "[ERROR] person to string buffer alloc");
+  snprintf(buffer, BUFFER_SIZE, "%s, age %d", item->name, item->age);
+  return buffer;
 }
 
 void showcase_list_float() {
-	list_float *origin = empty_list(float), *head = origin;
+  list_float *origin = empty_list(float), *head = origin;
 
-	const float f1 = 3.141, f2 = 2.71, f3 = 9.81;
-	head = append_float(head, &f1);
-	head = append_float(head, &f2);
-	head = append_float(head, &f3);
+  const float f1 = 3.141, f2 = 2.71, f3 = 9.81;
+  head = append_float(head, &f1);
+  head = append_float(head, &f2);
+  head = append_float(head, &f3);
 
-	printf("List of floats:\n");
-	printlst_float(head, float_to_string);
-	freelst_float(head);
+  printf("List of floats:\n");
+  printlst_float(head, float_to_string);
+  freelst_float(head);
 }
 
 void showcase_list_person() {
-	list_person *origin = empty_list(person), *head = origin;                                                     
-	const person p1 = { "John", 20 };
-	const person p2 = { "Jane", 18 };
-	const person p3 = { "William", 31 };
-	head = append_person(head, &p1);                            head = append_person(head, &p2);
-	head = append_person(head, &p3);
+  list_person *origin = empty_list(person), *head = origin;
+  const person p1 = {"John", 20};
+  const person p2 = {"Jane", 18};
+  const person p3 = {"William", 31};
+  head = append_person(head, &p1);
+  head = append_person(head, &p2);
+  head = append_person(head, &p3);
 
-	printf("List of people:\n");
-	printlst_person(head, person_to_string);
-	freelst_person(head);
+  printf("List of people:\n");
+  printlst_person(head, person_to_string);
+  freelst_person(head);
 }
 
 int main(void) {
 
-	showcase_list_float();
-	showcase_list_person();
-	
-	return EXIT_SUCCESS;
+  showcase_list_float();
+  showcase_list_person();
+
+  return EXIT_SUCCESS;
 }
