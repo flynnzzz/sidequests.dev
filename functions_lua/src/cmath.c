e3b5d724349c658dc85163e757c34d064e9d8074
flynnz <196103144+flynnzzz@users.noreply.github.com>
2026-09-04 15:04:43 +0200

    cleanup: removed unnecessary files

--- functions_lua/src/cmath.c
+++ functions_lua/src/cmath.c
@@ -1,4 +1,7 @@
-#include "cmath.h"
+#include <lua5.4/lauxlib.h>
+#include <lua5.4/lua.h>
+#include <lua5.4/lualib.h>
+#include <math.h>
 
 int c_pow(lua_State *L) {
   const double base = luaL_checknumber(L, 1), exp = luaL_checknumber(L, 2);
