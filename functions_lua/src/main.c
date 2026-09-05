f021c747d9e6de5ea9437e53089476474ee1ecb4
flynnz <196103144+flynnzzz@users.noreply.github.com>
2026-09-06 01:03:25 +0200

    refactor: better logging and minor corrections

--- functions_lua/src/main.c
+++ functions_lua/src/main.c
@@ -4,6 +4,7 @@
  * Startup the Lua Virtual Machine.
  */
 #include "luautils.h"
+#include <stdio.h>
 #define MAX_FUNCTIONS_NUM 128
 
 int main(void) {
@@ -14,9 +15,12 @@ int main(void) {
   lua_fn lua_funcs[MAX_FUNCTIONS_NUM];
   load_lua_fns(L, LUA_DIR_PATH, lua_funcs);
 
-  printf("executing functions:\n - f -> %.2f\n - g -> %.2f\n",
-         execute_lua_fn(L, lua_funcs[0].name, lua_funcs[0].nparams, 2.0, 2.0),
-         execute_lua_fn(L, lua_funcs[1].name, lua_funcs[1].nparams, 2.0));
+  const double res1 = execute_lua_fn(L, lua_funcs[0].name, lua_funcs[0].nparams,
+                                     2.0),
+               res2 = execute_lua_fn(L, lua_funcs[1].name, lua_funcs[1].nparams,
+                                     2.0, 2.0);
+  printf("executing functions:\n - %s -> %.2f\n - %s -> %.2f\n",
+         lua_funcs[0].name, res1, lua_funcs[1].name, res2);
 
   lua_close(L);
   return 0;
